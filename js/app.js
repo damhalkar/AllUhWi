364b17ccc51d63bf4167bac0ed06e32dfe8dc623
Halle <halle.reck@gmail.com>
2019-06-05 20:16:43 -0400

    Started localstorage

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 "use strict!";
+//Step 1 Get info from form
+//a. document.getElement
+//b. eventListener
+//c. handler
+//Step 2 Convert data
+//Step 3 Save to local storage
+
+var button = document.getElementById("contactInfo");
+button.addEventListener("submit", handler);
+console.log(button);
+
+function handler(event) {
+  event.preventDefault();
+  // var information = saveInfo(event.target.contactInfo.information );
+  console.log(event.target.firstname.value);
+  //assign correct positions
+  //var firstname
+  //var lastname
+  //call saveInfo
+  saveInfo();
+}
+
+function saveInfo(firstName, lastName, email, message) {
+  var contactInfo = {
+    first: firstName,
+    last: lastName,
+    email: email,
+    msg: message,
+  };
+  localStorage.setItem("data", JSON.stringify(contactInfo));
+}
