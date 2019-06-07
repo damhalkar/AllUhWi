d67ffa3dbf31b479acad6cf3d5be9d5a8b297db5
Kara Hunte <ncryted@gmail.com>
2019-06-07 18:13:48 -0400

    added additional structure for carousel

--- carousel.js
+++ carousel.js
@@ -1,21 +1,25 @@
 "use strict!";
 
-var slideIndex = 0;
-showSlides();
+var coverIndex = 0;
+showSlideshow();
 
-function showSlides() {
+function showSlideshow() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex =1}
+  
+//Math.floor(Math.random() * 4);
+  coverIndex++;
+  if (coverIndex > slides.length) {coverIndex =1}
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace("active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000);
+  slides[coverIndex-1].style.display = "block";  
+  console.log(coverIndex);
+  dots[coverIndex-1].className += " active";
+  setTimeout(showSlideshow, 3000);
+  //document.addEventListener(type,reset)
 }
