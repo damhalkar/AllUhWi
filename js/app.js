4208571c3dd83202326115815241571e124688cd
dg-griff <dg4.griff@gmail.com>
2019-06-09 23:59:37 -0400

    Minor fixes to index page layout and styling.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 "use strict!";
+
+function myFunction() {
+    var hideContent = document.getElementById("hide");
+      if (hideContent.style.display === "none") {
+        hideContent.style.display = "block";
+      } else {
+        hideContent.style.display = "none";
+      }
+}
